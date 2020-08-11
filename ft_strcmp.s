5c511c5559f9468b2be163e41dc1e3507d732a17
YoukjinLee <ukjinlee66@gmail.com>
2020-08-11 20:34:26 +0900

    fin

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,4 +2,16 @@ global _ft_strcmp
 
 section .text
 _ft_strcmp :
-		cmp
+		push	rbp
+		mov		rbp, rsp
+		sub		rsp, 32
+		mov		[rbp - 8], rdi
+		mov		[rbp - 16], rsi
+		mov		qword [rbp - 24], 0
+		mov		rbx, [rbp - 8]
+		mov		rcx, [rbp - 24]
+		mov		rdx, [rbp - 16]
+loop :
+		cmp		byte [rax + rcx], 0
+		jmp		test
+		
